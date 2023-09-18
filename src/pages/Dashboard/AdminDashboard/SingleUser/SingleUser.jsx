9beea651903c196f0c5892cd6e71ffb3439bea44
himaloyash <himaloyashis98@gmail.com>
2023-09-18 23:22:23 +0600

    edited

--- src/pages/Dashboard/AdminDashboard/SingleUser/SingleUser.jsx
+++ src/pages/Dashboard/AdminDashboard/SingleUser/SingleUser.jsx
@@ -8,7 +8,7 @@ const SingleUser = ({user}) => {
         <tr>
             
             {/* <td>
-                <div className="flex items-center space-x-3">
+                <div className="flex items-center o space-x-3">
                     <div className="avatar">
                         <div className="mask mask-squircle w-12 h-12">
                             <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
