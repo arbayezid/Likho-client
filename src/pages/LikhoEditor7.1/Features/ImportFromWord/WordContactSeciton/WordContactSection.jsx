f63097e046e8672bf4a6eaf66168a7a51d2c52a6
joySutradhor <joysutradhorcmt@gmail.com>
2023-09-20 03:18:43 +0600

    import pdf section 70 parcent done

---
+++ src/pages/LikhoEditor7.1/Features/ImportFromWord/WordContactSeciton/WordContactSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WordContactSection = () => {
+    return (
+        <div className='bg-gradient-to-r from-gray-900 via-purple-950 to-purple-800 flex flex-col justify-center items-center py-16 md:mx-20 mx-10 rounded-md'>
+            <h1 className='text-4xl text-white font-bold'>Want to learn more about Likho Editor?</h1>
+            <button className='bg-[#C9FE43] px-8 py-4 mt-8 text-xl font-bold rounded-md'> Contact Us </button>
+        </div>
+    );
+};
+
+export default WordContactSection;
