356e9e23d9c793c35406d77d24e0048e87b6b6a1
ataullahmesbah <mesbahataullah4@gmail.com>
2023-09-20 20:03:11 +0600

    creative res

--- src/pages/Template/Templates.jsx
+++ src/pages/Template/Templates.jsx
@@ -19,7 +19,7 @@ const Templates = ({ template }) => {
     return (
         <div className="font-poppins pt-8 space-y-5 w-full lg:w-4/5 lg:flex lg:flex-col lg:justify-start lg:items-start">
             <div
-                className={`bg-gray-200 rounded-2xl justify-center ${isHovered ? "transform scale-105 transition-transform" : ""
+                className={`bg-gray-200 border-s-8  border-purple-900 rounded-2xl justify-center ${isHovered ? "transform scale-105 transition-transform" : ""
                     }`}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
@@ -29,11 +29,15 @@ const Templates = ({ template }) => {
             <div className="space-y-5 px-2">
                 <h2 className="lg:text-xl font-semibold text-blue-950 ">{title}</h2>
                 <p className="text-gray-700">{slicedDescription}...</p>
-                <div className="border-b-0">
+                <div className="border-b-0 justify-end items-center mt-auto" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'flex-start' }}>
                     <Link to={`/templatesdetails/${_id}`}>
-                        <button className="border border-gray-400 text-base lg:text-lg font-poppins px-2 bg-blue-100 hover:bg-gray-200 text-purple-800 p-1 rounded-lg">
-                            Try Demo
-                        </button>
+                        <div className="space-y-2">
+                            <button
+                                className="text-base lg:text-lg font-poppins px-2 bg-blue-100 hover:bg-gray-200 text-purple-800 p-1 rounded-lg border border-gray-400 border-b-2"
+                            >
+                                Try Demo
+                            </button>
+                        </div>
                     </Link>
                 </div>
             </div>
